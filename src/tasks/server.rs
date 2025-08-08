00fcb5bf902c85a4e6005b35491dec4491e4cf66
martinsookael <martinsookael@gmail.com>
2025-08-08 19:50:02 +0300

    move port 9010 by default

--- src/tasks/server.rs
+++ src/tasks/server.rs
@@ -2,7 +2,7 @@ use crate::tasks::handle_lv::handle_lv;
 use actix_web::{App, HttpServer, web};
 
 pub async fn start_server() -> std::io::Result<()> {
-    let address = "127.0.0.1:8080";
+    let address = "127.0.0.1:9010";
     println!("KYB server: {}", &address);
     HttpServer::new(|| App::new().route("/lv", web::post().to(handle_lv)))
         .bind(address)?
