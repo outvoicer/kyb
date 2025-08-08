bc34475c082219995c1e44df5e8c0d299c7d763e
martinsookael <martinsookael@gmail.com>
2025-08-08 21:21:37 +0300

    better db location

--- src/db/db_file.rs
+++ src/db/db_file.rs
@@ -2,7 +2,7 @@ use crate::db::app_folder::app_folder;
 use crate::error::KybError;
 use std::path::PathBuf;
 
-const COMPANY_DB: &str = "finance.db";
+const COMPANY_DB: &str = "kyb.db";
 
 pub fn db_file() -> Result<PathBuf, KybError> {
     let folder = app_folder()?;
