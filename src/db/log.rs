d0a02092b8f6943d9be69a38e3c18327e496cf43
martinsookael <martinsookael@gmail.com>
2025-08-09 13:27:23 +0300

    log time more human readable

--- src/db/log.rs
+++ src/db/log.rs
@@ -9,13 +9,14 @@ pub fn log(
     error: Option<&String>,
 ) -> Result<i64, KybError> {
     let now = Local::now();
+    let formatted_time = now.format("%Y-%m-%d %H:%M:%S").to_string();
     let err = match error {
         Some(err) => err,
         None => "",
     };
     conn.execute(
         "INSERT INTO log (decition, name, personal_code, reg_code, error, time) VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
-        params![decition, &query.name, &query.personal_code, query.reg_code, err, now.to_string()],
+        params![decition, &query.name, &query.personal_code, query.reg_code, err, formatted_time],
     )?;
     let last_id = conn.last_insert_rowid();
     Ok(last_id)
