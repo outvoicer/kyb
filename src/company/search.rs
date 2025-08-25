872cbbadad19f1ebd4f6dbaf31846f9124be0278
martinsookael <martinsookael@gmail.com>
2025-08-25 23:04:46 +0300

    get precice link first

--- src/company/search.rs
+++ src/company/search.rs
@@ -12,7 +12,16 @@ impl Company {
         name: &String,
     ) -> Result<Vec<Company>, Box<dyn Error>> {
         let mut stmt = conn.prepare(
-            "SELECT legal_form, name, city, address, zip, public_sector, reg_code, vat, vat_number FROM company WHERE normal_name LIKE ('%' || ?1 || '%') LIMIT 10"
+            "SELECT legal_form, name, city, address, zip, public_sector, reg_code, vat, vat_number
+            FROM company
+            WHERE normal_name
+            LIKE ('%' || ?1 || '%')
+            ORDER BY
+                     CASE
+                         WHEN normal_name = ?1 THEN 0  -- Exact match gets highest priority
+                         ELSE 1  -- Partial matches get lower priority
+                     END
+             LIMIT 10 ",
         )?;
         // REMOVE SIA FROM BEGINNING
         let clean_name = clean_company_name(&name);
@@ -115,10 +124,10 @@ mod tests {
         assert_eq!(result.reg_code, "50103563161".to_string());
         assert_eq!(result.legal_form, "SIA".to_string());
         //SIA iS REMOVED FROM BEGINNING, RETURNS FIRST COMPANY THAT HAS R
-        // let search_term = "SIA R".to_string();
-        // let result = get_first_result(&conn, &search_term).await.unwrap();
-        // assert_eq!(result.reg_code, "40008234596".to_string());
-        // AS Something Liepsaime
+        let search_term = "SIA R".to_string();
+        let result = get_first_result(&conn, &search_term).await.unwrap();
+        assert_eq!(result.reg_code, "40008234596".to_string());
+        // AS removed from beginning
         let search_term = "AS Liepsaime".to_string();
         let result = get_first_result(&conn, &search_term).await.unwrap();
         assert_eq!(result.reg_code, "40203179017".to_string());
@@ -128,12 +137,10 @@ mod tests {
         assert_eq!(result.vat, true);
         assert_eq!(result.vat_number, Some("LV90000069281".to_string()));
         // TBD VID
-        /*
+
         // TBD - SEND PRECICE MATCH FIRST
         let search_term = "Laik".to_string();
         let result = get_first_result(&conn, &search_term).await.unwrap();
-        println!("{:?}", result);
-        assert_eq!(result.reg_code, "40203179017".to_string());
-        */
+        assert_eq!(result.reg_code, "40003922099".to_string());
     }
 }
