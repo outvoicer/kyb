4753c450af646bcb041df35832c4d9388d587fe3
martinsookael <martinsookael@gmail.com>
2026-02-18 09:58:43 +0200

    iterate api/test responder

--- src/server/api_test.rs
+++ src/server/api_test.rs
@@ -1,35 +1,32 @@
 use crate::{db::get_db::Pool, latvia::company::company::Company};
 use actix_web::{HttpResponse, Responder, web};
-use r2d2::PooledConnection;
-use r2d2_sqlite::SqliteConnectionManager;
 
 pub async fn api_test(pool: web::Data<Pool>) -> impl Responder {
-    // GET DB
-    let db: PooledConnection<SqliteConnectionManager> =
-        pool.get().expect("Couldn't get db connection from pool");
-    // SEARCH FOR RAIMOND
+    let db = match pool.get() {
+        Ok(conn) => conn,
+        Err(err) => {
+            eprintln!("Failed to get DB connection: {}", err);
+            return HttpResponse::InternalServerError()
+                .json(serde_json::json!({ "error": "Failed to get DB connection" }));
+        }
+    };
+
     match Company::search_by_name(&db, &"Raimond Fantastic".to_string(), true).await {
         Ok(results) => {
-            // IS THERE FIRST RESULT
             if let Some(first_result) = results.get(0) {
-                // IS IT RAIMOND FANTASTIC
                 if first_result.reg_code == "40203572370" {
-                    return HttpResponse::Ok().json(serde_json::json!({ "test": true }));
+                    HttpResponse::Ok().json(serde_json::json!({ "test": true }))
                 } else {
-                    // RAIMOND IS NOT THERE - DB IS EMPTY OR SOMETHING BAD HAS HAPPENED
-                    return HttpResponse::BadRequest()
-                        .json(serde_json::json!({ "error": "DB empty maybe" }));
+                    HttpResponse::BadRequest()
+                        .json(serde_json::json!({ "error": "DB empty maybe" }))
                 }
             } else {
-                // MOST LIKELY EMPTY DB
-                return HttpResponse::NotFound().json(serde_json::json!({ "error": "DB empty" }));
+                HttpResponse::NotFound().json(serde_json::json!({ "error": "DB empty" }))
             }
         }
         Err(err) => {
-            // OTHER ERRORS WITH SEARCH
-            eprintln!("test err: {}", err);
-            return HttpResponse::InternalServerError()
-                .json(serde_json::json!({ "error": "Server down" }));
+            eprintln!("Search error: {}", err);
+            HttpResponse::InternalServerError().json(serde_json::json!({ "error": "Server down" }))
         }
-    };
+    }
 }
